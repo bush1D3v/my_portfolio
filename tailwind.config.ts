8647bd81133d4e6e551db1048347d1dcc15db4d8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-18 23:39:46 -0300

    build(tailwind.config.js): Adiciona variaveis de cor ao tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blueBase: "var(--blue-base)",
+        darkBlue: "var(--dark-blue)",
+        lightBlue: "var(--light-blue)",
+        saturatedBlue: "var(--saturated-blue)",
+        desaturatedBlue: "var(--desaturated-blue)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+        black: "var(--black)",
+        text: "var(--white)",
+        error: "var(--error)",
+        input: "var(--input)",
+        darkGray: "var(--dark-gray)",
+        lightGray: "var(--light-gray)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
