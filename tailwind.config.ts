8ce1f414f346901b3219b7882dd518ce58042dcf
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-20 21:11:20 -0300

    build(tailwind.config.js): Removendo variaveis de cor nao utilizadas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,14 +11,9 @@ const config: Config = {
         darkBlue: "var(--dark-blue)",
         lightBlue: "var(--light-blue)",
         saturatedBlue: "var(--saturated-blue)",
-        desaturatedBlue: "var(--desaturated-blue)",
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
         accent: "var(--accent)",
         black: "var(--black)",
-        text: "var(--white)",
-        error: "var(--error)",
-        input: "var(--input)",
+        white: "var(--white)",
         darkGray: "var(--dark-gray)",
         lightGray: "var(--light-gray)",
       },
