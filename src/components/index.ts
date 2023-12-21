dc08737271998d1d2c925f8fbd6a792f10dba3c5
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-20 21:18:01 -0300

    feat(index.ts): Adiciona index exportador de componentes

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Header from "./Header/Header";
+import Footer from "./Footer";
+import LinkButton from "./LinkButton";
+import Profile from "./Profile";
+import EmbedPost from "./EmbedPost";
+import Logo from "./Logo";
+
+export {
+  Header,
+  Footer,
+  LinkButton,
+  Profile,
+  EmbedPost,
+  Logo
+};
