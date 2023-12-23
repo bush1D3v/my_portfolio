3836c27d823f3acddda5be33d8897ce93ed52127
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:58:06 -0300

    update(index.ts): Atualiza exportacao de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer";
-import LinkButton from "./LinkButton";
-import Profile from "./Profile";
 
 export {
   Header,
   Footer,
-  LinkButton,
-  Profile,
 };
