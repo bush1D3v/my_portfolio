d278a12c2a027f7f51e7aa9a2926d98b560cb2cf
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:45:15 -0300

    feat(page.tsx): Cria componente de pagina skills

---
+++ src/app/skills/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+export default function Skills(): ReactElement {
+  return (
+    <div
+      className="flex items-center justify-center h-[90dvh]"
+    >
+      <h1>Habilidades</h1>
+    </div>
+  );
+}
