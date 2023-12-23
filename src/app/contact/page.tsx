bd369e3453b0ba0b6d19328b39391326af1438ec
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:54:32 -0300

    feat(page.tsx): Cria componente de pagina contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import { type ReactElement } from "react";
+
+export default function Contact(): ReactElement {
+  return (
+    <div
+      className="flex items-center justify-center h-[90dvh]"
+    >
+      <h1>Contato</h1>
+    </div>
+  );
+}
