4aed6c2f9373e33681838c23f379e9084f1188e6
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:47:52 -0300

    feat(page.tsx): Cria componente de pagina formations

---
+++ src/app/formations/page.tsx
@@ -0,0 +1,11 @@
+import { type ReactElement } from "react";
+
+export default function Formations(): ReactElement {
+  return (
+    <div
+      className="flex items-center justify-center h-[90dvh]"
+    >
+      <h1>Formations</h1>
+    </div>
+  );
+}
