d835a0287bf72b29e599755aba985cfc8788fb2a
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:42:41 -0300

    feat(Header.tsx): Cria header padrao do projeto

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,33 @@
 import { type ReactElement } from "react";
+import Image from "next/image";
+
+import { ListItem } from "./components";
+import logo from "../../../public/logo.png";
 
 export default function Header(): ReactElement {
-  return <header className="px-1 h-[5dvh] flex items-center"></header>;
+  return (
+    <header
+      className="lg:mt-[3dvh] p-5 flex items-center justify-around max-w-screen-xl h-[9dvh] m-auto bg-Black2 rounded-xl"
+    >
+      <div className="flex hover:opacity-50 cursor-pointer ease-in-out duration-150">
+        <Image
+          src={logo}
+          alt="Logo"
+          className="dark:brightness-100 brightness-0 w-20 cursor-pointer"
+        />
+      </div>
+      <nav className="flex">
+        <ul className="flex gap-20 justify-center items-center text-center w-full text-White">
+          <ListItem href="/about" text="Sobre" />
+          <ListItem href="/skills" text="Habilidades" />
+          <ListItem href="/projects" text="Projetos" />
+          <ListItem href="/certifications" text="Certificações" />
+          <ListItem href="/contact" text="Contato" />
+        </ul>
+      </nav>
+      <button>
+        ButtonTheme
+      </button>
+    </header>
+  );
 }
