623c730a4a06e3929afac18cf207878337dae266
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-23 17:49:25 -0300

    feat(Logo.tsx): cria componente para logo no header

---
+++ src/components/Header/components/Logo.tsx
@@ -0,0 +1,21 @@
+import { type ReactElement } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import logo from "../../../../public/logo.png";
+
+export default function Logo(): ReactElement {
+  return (
+    <div className="flex hover:opacity-50 cursor-pointer ease-in-out duration-150">
+      <Link
+        href={"/"}
+      >
+        <Image
+          src={logo}
+          alt="Logo"
+          className="dark:brightness-100 brightness-0 w-20 cursor-pointer"
+        />
+      </Link>
+    </div>
+  );
+}
