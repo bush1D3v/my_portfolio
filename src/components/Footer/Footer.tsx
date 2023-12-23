66142a11bf2deda2ab95d4b5f86c2a2b76736c4e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:42:52 -0300

    feat(Footer.tsx): Cria footer padrao do projeto

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,30 @@
 import { type ReactElement } from "react";
+import {
+  FaGithub,
+  FaDiscord,
+  FaWhatsapp,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin
+} from "react-icons/fa";
+
+import { LinkItem } from "./components";
 
 export default function Footer(): ReactElement {
   return (
-    <footer className="h-[5dvh] flex gap-4 items-center justify-center relative">
-      <p className="font-light absolute left-4">© 2023</p>
-      <p>Made with 💖 by&nbsp;
-        <a
-          href="https://github.com/bush1D3v"
-          className="underline hover:text-saturatedBlue duration-300 ease-in-out"
-          target="_blank"
-          rel="noreferrer"
-        >
-          bush1D3v
-        </a>
-      </p>
+    <footer
+      className="lg:mb-[3dvh] p-5 flex items-center justify-center max-w-screen-xl h-[9dvh] m-auto bg-Black2 rounded-xl"
+    >
+      <address>
+        <ul className="flex gap-5">
+          <LinkItem href="https://github.com/bush1D3v" icon={<FaGithub />} />
+          <LinkItem href="https://www.linkedin.com/in/vj021/" icon={<FaLinkedin />} />
+          <LinkItem href="https://discord.com/users/443480311066656770" icon={<FaDiscord />} />
+          <LinkItem href="https://www.instagram.com/victorj.021" icon={<FaInstagram />} />
+          <LinkItem href="https://api.whatsapp.com/send?phone=5521995727079" icon={<FaWhatsapp />} />
+          <LinkItem href="https://twitter.com/victorj_021" icon={<FaTwitter />} />
+        </ul>
+      </address>
     </footer>
   );
 }
