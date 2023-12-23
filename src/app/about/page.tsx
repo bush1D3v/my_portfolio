14823ab6f7acbebe1c2403708928c0094a46bb5f
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-22 23:45:07 -0300

    feat(page.tsx): Cria componente de pagina about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import { type ReactElement } from "react";
+
+export default function About(): ReactElement {
+  return (
+    <div
+      className="flex items-center justify-center h-[90dvh]"
+    >
+      <h1>Sobre</h1>
+    </div>
+  );
+}
