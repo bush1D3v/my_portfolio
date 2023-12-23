bf3f30dd31bd3a35886cc1686ae00d4c4367628e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-23 17:49:56 -0300

    update(Header.tsx): Refatorando criacao de logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,13 @@
 import { type ReactElement } from "react";
-import Image from "next/image";
 
-import { ListItem } from "./components";
-import logo from "../../../public/logo.png";
+import { ListItem, Logo } from "./components";
 
 export default function Header(): ReactElement {
   return (
     <header
       className="lg:mt-[3dvh] p-5 flex items-center justify-around max-w-screen-xl h-[9dvh] m-auto bg-Black2 rounded-xl"
     >
-      <div className="flex hover:opacity-50 cursor-pointer ease-in-out duration-150">
-        <Image
-          src={logo}
-          alt="Logo"
-          className="dark:brightness-100 brightness-0 w-20 cursor-pointer"
-        />
-      </div>
+      <Logo />
       <nav className="flex">
         <ul className="flex gap-20 justify-center items-center text-center w-full text-White">
           <ListItem href="/about" text="Sobre" />
