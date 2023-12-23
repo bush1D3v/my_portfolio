4c6097177ad0226381dc1b7f2a00e335d74effe8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-23 17:50:41 -0300

    update(page.tsx): Atualiza altura do componente

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { ReactElement } from "react";
 
 export default function Skills(): ReactElement {
   return (
     <div
-      className="flex items-center justify-center h-[90dvh]"
+      className="flex items-center justify-center h-[82dvh] lg:h-[76dvh]"
     >
       <h1>Habilidades</h1>
     </div>
