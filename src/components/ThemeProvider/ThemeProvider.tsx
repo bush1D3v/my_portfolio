79ed92441d61173bb39625824abe7461f4bbf670
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-24 13:37:01 -0300

    feat(ThemeProvider.tsx): Cria componente provedor de tema de usuario mantendo tudo em SSR

---
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { type ReactElement } from "react";
+
+export default function ThemeProvider({
+  children }: React.PropsWithChildren
+): ReactElement {
+  return (
+    <NextThemeProvider>
+      {children}
+    </NextThemeProvider>
+  );
+}
