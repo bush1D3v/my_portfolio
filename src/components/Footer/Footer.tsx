cc4754a10be79d43136377d9fc43c73998c5a1fa
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-25 01:48:07 -0300

    update(Footer.tsx): Adicionando informacoes de email e localizacao

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,10 +13,26 @@ import { LinkItem } from "./components";
 export default function Footer(): ReactElement {
   return (
     <footer
-      className="lg:mb-[3dvh] p-5 flex items-center justify-center max-w-screen-xl
-      h-[9dvh] m-auto bg-Black2 rounded-xl duration-200 ease-in"
+      className="lg:mb-[3dvh] h-[22dvh] md:h-[9dvh] flex flex-col md:flex-row items-center justify-between
+      md:justify-around m-auto bg-Black2 max-w-screen-xl lg:rounded-xl duration-200 ease-in
+      p-5 animate-fade-down animate-ease-in-out"
     >
+      <address className="flex md:flex-col w-full lg:w-fit flex-start gap-4 md:gap-2">
+        <p className="text-White duration-200">Email</p>
+        <a
+          href="mailto:victor.jose.lopes.navarro@gmail.com"
+          target="_blank"
+          className="hover:opacity-50"
+        >
+          victor.jose.lopes.navarro@gmail.com
+        </a>
+      </address>
+      <address className="flex md:flex-col w-full md:text-center lg:w-fit flex-start gap-4 md:gap-2">
+        <p className="text-White duration-200">Localização</p>
+        <p>Rio de Janeiro</p>
+      </address>
       <address>
+        <div></div>
         <ul className="flex gap-5">
           <LinkItem href="https://github.com/bush1D3v" icon={<FaGithub />} />
           <LinkItem href="https://www.linkedin.com/in/vj021/" icon={<FaLinkedin />} />
