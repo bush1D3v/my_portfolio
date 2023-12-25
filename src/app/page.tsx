13d9d45bd5a6bc262c2d049bcd23cb3094c6af0b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-25 01:50:03 -0300

    update(page.tsx): Atualiza altura do componente e adiciona animacao de entrada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import { type ReactElement } from "react";
 
 export default function Home(): ReactElement {
   return (
-    <main className="flex flex-col items-center justify-center h-[82dvh] lg:h-[76dvh]">
-      <h1 className="flex">
+    <main
+      className="flex flex-col items-center justify-center h-[69dvh] md:h-[82dvh] lg:h-[76dvh]
+      animate-fade-down animate-ease-in-out"
+    >
+      <h1 className="flex ease-in duration-75">
         Olá Mundo!
         <span className="animate-wiggle-more animate-infinite animate-ease-in-out">👋</span>
       </h1>
-      <h2>Seja bem-vindo(a) ao meu portfólio!</h2>
+      <h2 className="ease-in duration-75 text-center">Seja bem-vindo(a) ao meu portfólio!</h2>
     </main>
   );
 }
