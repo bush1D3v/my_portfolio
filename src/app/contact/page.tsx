29c809b0006c93bfc6f7b8329d5792eb9fd59c63
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-25 01:50:19 -0300

    update(page.tsx): Atualiza altura do componente e adiciona animacao de entrada

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,11 @@ import { type ReactElement } from "react";
 
 export default function Contact(): ReactElement {
   return (
-    <div
-      className="flex items-center justify-center h-[82dvh] lg:h-[76dvh]"
+    <main
+      className="flex items-center justify-center h-[69dvh] md:h-[82dvh] lg:h-[76dvh] animate-fade-down
+      animate-ease-in-out"
     >
       <h1>Contato</h1>
-    </div>
+    </main>
   );
 }
