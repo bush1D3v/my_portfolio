eaeeaecaa8f34044c0afe1301c51f0204595a300
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-25 01:49:37 -0300

    update(index.ts): Atualiza exportacao de componentes do header

--- src/components/Header/components/index.ts
+++ src/components/Header/components/index.ts
@@ -1,9 +1,11 @@
 import ListItem from "./ListItem";
 import Logo from "./Logo";
 import ButtonTheme from "./ButtonTheme";
+import NavBar from "./Navbar";
 
 export {
   ListItem,
   Logo,
-  ButtonTheme
+  ButtonTheme,
+  NavBar
 };
