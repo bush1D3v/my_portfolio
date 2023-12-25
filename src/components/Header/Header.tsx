356303a796d9e9b8b77217192e716320c0cd5d2b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-25 01:48:26 -0300

    update(Header.tsx): Refatorando componente de navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,14 @@
 import { type ReactElement } from "react";
 
-import { ButtonTheme, ListItem, Logo } from "./components";
+import { NavBar } from "./components";
 
 export default function Header(): ReactElement {
   return (
     <header
-      className="lg:mt-[3dvh] p-5 flex items-center justify-around max-w-screen-xl ]
-      h-[9dvh] m-auto bg-Black2 rounded-xl duration-200 ease-in"
+      className="bg-Black2 md:h-[9dvh] lg:rounded-xl lg:mt-[3dvh] max-w-screen-xl animate-fade-down animate-ease-in-out
+      duration-200 ease-in m-auto"
     >
-      <Logo />
-      <nav className="flex">
-        <ul className="flex gap-20 justify-center items-center text-center w-full text-White">
-          <ListItem href="/about" text="Sobre" />
-          <ListItem href="/skills" text="Habilidades" />
-          <ListItem href="/projects" text="Projetos" />
-          <ListItem href="/formations" text="Formações" />
-          <ListItem href="/contact" text="Contato" />
-        </ul>
-      </nav>
-      <ButtonTheme />
+      <NavBar />
     </header>
   );
 }
