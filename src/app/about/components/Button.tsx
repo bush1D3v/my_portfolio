ed97e43fb4b7b2a4e99bbead0466401a0598eb1d
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-29 15:19:20 -0300

    feat(Button.tsx): Cria componente de botao para a pagina about

--- src/app/about/components/Button.tsx
+++ src/app/about/components/Button.tsx
@@ -5,17 +5,28 @@ interface ButtonProps {
   link: string;
   text: string;
   icon: ReactElement;
+  border: string;
+  hover: string;
 }
 
 export default function Button(
-  { link, text, icon }: ButtonProps
+  {
+    link,
+    text,
+    icon,
+    border,
+    hover
+  }: ButtonProps
 ): ReactElement {
+  const className = `text-White font-semibold uppercase flex items-center justify-center duration-200 ease-in px-20 py-8
+  cursor-pointer border-${border} border-2 hover:border-${hover} rounded-3xl`;
+
   return (
     <Link
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-White font-semibold uppercase flex items-center justify-center duration-200 ease-in"
+      className={className}
     >
       {text} &nbsp; {icon}
     </Link>
