1dc40adc5d33e929e132b59a18540c197894d870
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-30 21:38:27 -0300

    feat(index.ts): Cria index exportador de componentes do NavBar

---
+++ src/components/Header/components/NavBar/components/index.ts
@@ -0,0 +1,11 @@
+import ListItem from "./ListItem";
+import Logo from "./Logo";
+import ButtonTheme from "./ButtonTheme";
+import MenuSvg from "./MenuSvg";
+
+export {
+  ListItem,
+  Logo,
+  ButtonTheme,
+  MenuSvg
+};
