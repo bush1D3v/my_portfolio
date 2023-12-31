9462944137d05d5bb9ec74b450f38c2434ed9f62
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-30 21:36:12 -0300

    update(index.ts): Atualiza exportacao de componentes globais

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer";
 import ThemeProvider from "./ThemeProvider/index";
+import Modal from "./Modal";
 
 export {
   Header,
   Footer,
-  ThemeProvider
+  ThemeProvider,
+  Modal
 };
