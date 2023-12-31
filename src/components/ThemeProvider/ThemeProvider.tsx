8aa23859a6c1a8d3b5f09214b0490562ecfbf426
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-12-30 21:30:55 -0300

    refactor(ThemeProvider.tsx): Refatorando identacao de tipagens da funcao

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -4,8 +4,8 @@ import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { type ReactElement } from "react";
 
 export default function ThemeProvider({
-  children }: React.PropsWithChildren
-): ReactElement {
+  children
+}: React.PropsWithChildren): ReactElement {
   return (
     <NextThemeProvider>
       {children}
