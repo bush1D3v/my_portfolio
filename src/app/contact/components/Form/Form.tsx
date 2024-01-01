debacdd121a804518631e0d8871f305b2098c0bb
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 19:31:13 -0300

    feat(Form.tsx): Cria componente de formulario

---
+++ src/app/contact/components/Form/Form.tsx
@@ -0,0 +1,47 @@
+import { type ReactElement } from "react";
+import { AiFillMessage } from "react-icons/ai";
+
+import { LinkButton } from "@/components";
+
+import { LabelInput } from "./components";
+
+
+export default function Form(): ReactElement {
+  return (
+    <form
+      action="https://formspree.io/f/xzbnbrdj"
+      method="POST"
+      className="flex flex-col gap-6 items-start w-fit"
+    >
+      <LabelInput
+        type={"text"}
+        name={"nome"}
+        placeholder={"Seu Nome"}
+      />
+      <LabelInput
+        type={"email"}
+        name={"email"}
+        placeholder={"Seu Email"}
+      />
+      <label className="w-full">
+        <textarea
+          name="mensagem"
+          placeholder="Sua Mensagem"
+          className="duration-200 ease-in border-2 border-RomanSilver rounded-2xl px-8 py-4
+          bg-Gray2 font-semibold resize-none w-full"
+          cols={30}
+          rows={3}
+          required
+        />
+      </label>
+      <LinkButton
+        link={null}
+        target={"_self"}
+        type={"submit"}
+        text={"ENVIAR MENSAGEM"}
+        icon={<AiFillMessage />}
+        additionalClass={"border-White hover:border-Gray"}
+      />
+    </form>
+  );
+}
