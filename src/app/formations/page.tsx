c594ee8760c157035c411f70bd94cafee5e629b4
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 20:15:39 -0300

    update(page.tsx): Alterando pagina para SSR

--- src/app/formations/page.tsx
+++ src/app/formations/page.tsx
@@ -1,27 +1,14 @@
-"use client";
-
 import { type ReactElement } from "react";
 
-import { loaderSessionStorage } from "@/functions";
-
-import Loading from "../loading";
 import { FormationsList } from "./components";
 
 export default function Formations(): ReactElement {
-  const isLoading: boolean = loaderSessionStorage("formationsLoaded");
-
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <main
-          className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
-          animate-ease-in-out max-w-screen-xl p-10 m-auto"
-        >
-          <FormationsList />
-        </main>
-      )}
-    </>
+    <main
+      className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
+      animate-ease-in-out max-w-screen-xl p-10 m-auto"
+    >
+      <FormationsList />
+    </main>
   );
 }
