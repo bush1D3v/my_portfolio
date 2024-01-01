9d34bd1fd2e449ffd5a45db56cf6bcddfb442e12
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 19:26:48 -0300

    update(Photo.tsx): Adicionando key para alt e melhorando renderizacao de imagem

--- src/app/about/components/Photo.tsx
+++ src/app/about/components/Photo.tsx
@@ -1,17 +1,17 @@
 import { type ReactElement } from "react";
 import Image from "next/image";
 
-export default function Logo(): ReactElement {
+export default function Photo(): ReactElement {
   return (
     <div className="flex h-full lg:w-full items-center justify-center">
       <Image
         src="https://avatars.githubusercontent.com/u/133554156?v=4"
         className="rounded-3xl rounded-br-[15%] md:rounded-bl-[7%] md:rounded-t-[17%] md:rounded-br-[30%] w-[80%]"
-        alt="teste"
+        alt="Foto de perfil"
         priority={true}
         quality={100}
-        width={500}
-        height={500}
+        width={400}
+        height={400}
         decoding="async"
       />
     </div>
