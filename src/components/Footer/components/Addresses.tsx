60817af79cc6e1a957118f8c54d303c3ea662aab
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 19:28:26 -0300

    refactor(Addresses.tsx): Altera tag de a para Link

--- src/components/Footer/components/Addresses.tsx
+++ src/components/Footer/components/Addresses.tsx
@@ -1,17 +1,19 @@
 import { type ReactElement } from "react";
+import Link from "next/link";
 
 export default function Addresses(): ReactElement {
   return (
     <>
       <address className="flex md:flex-col w-full lg:w-fit flex-start gap-4 md:gap-2">
         <p className="text-White duration-200">Email</p>
-        <a
+        <Link
           href="mailto:victor.jose.lopes.navarro@gmail.com"
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:opacity-50"
         >
           victor.jose.lopes.navarro@gmail.com
-        </a>
+        </Link>
       </address>
       <address className="flex md:flex-col w-full md:text-center lg:w-fit flex-start gap-4 md:gap-2">
         <p className="text-White duration-200">Localização</p>
