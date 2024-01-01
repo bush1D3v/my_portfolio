baac82e99483dbaf04e498b9da12ff1c1f199ef9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 19:29:17 -0300

    update(Logo.tsx): Adiciona valor para a key rel do Link

--- src/components/Header/components/NavBar/components/Logo.tsx
+++ src/components/Header/components/NavBar/components/Logo.tsx
@@ -2,13 +2,14 @@ import { type ReactElement } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "@/assets/logo.png";
+import { logo } from "@/assets";
 
 export default function Logo(): ReactElement {
   return (
     <div className="flex hover:opacity-50 cursor-pointer ease-in-out duration-150">
       <Link
         href={"/"}
+        rel="noopener noreferrer"
       >
         <Image
           src={logo}
