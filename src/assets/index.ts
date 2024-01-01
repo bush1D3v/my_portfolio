3fedd1f6efb8a1845cd445b694c5a06f3f84fbaa
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 19:37:40 -0300

    feat(index.ts): Cria index exportador de imagens

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import cubosAcademy from "./cubos-academy.jpg";
+import cursoEmVideo from "./curso-em-video.jpg";
+import devQuest from "./dev-quest.jpg";
+import dio from "./dio.jpg";
+import logo from "./logo.png";
+
+export {
+  cubosAcademy,
+  cursoEmVideo,
+  devQuest,
+  dio,
+  logo
+};
