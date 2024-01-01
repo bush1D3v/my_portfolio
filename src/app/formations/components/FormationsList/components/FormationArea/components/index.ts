488203e2283caaea62dadd60637e4be2387dbe2c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-01 19:41:20 -0300

    feat(index.ts): Cria exportador de componentes do FormationArea

---
+++ src/app/formations/components/FormationsList/components/FormationArea/components/index.ts
@@ -0,0 +1,9 @@
+import ArrowUp from "./ArrowUp";
+import FormationTechnologiesSummary from "./FormationTechnologiesSummary";
+import ImageWithHiddenDescription from "./ImageWithHiddenDescription";
+
+export {
+  ImageWithHiddenDescription,
+  FormationTechnologiesSummary,
+  ArrowUp
+};
