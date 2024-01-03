2b63cbeab4a459b563e16ccf1049db391da0acdd
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-02 22:42:10 -0300

    fix(not-found.tsx): Corrigindo tamanho de fontes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,8 @@ import { LinkButton } from "@/components";
 export default function NotFound(): ReactElement {
   return (
     <main className="h-[69dvh] md:h-[82dvh] lg:h-[76dvh] flex flex-col items-center justify-center gap-8 text-center">
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium duration-200 ease-in">Eita! 🫢</h1>
-      <p className="font-bold text-2xl md:text-3xl lg:text-4xl px-6 md:px-10 lg:px-14 duration-200 ease-in">
+      <h1 className="duration-200 ease-in">Eita! 🫢</h1>
+      <p className="font-semibold text-2xl md:text-3xl lg:text-4xl px-6 md:px-14 duration-200 ease-in text-White">
         Parece que você tentou acessar uma página que não existe, não é mesmo? 🤔<br />
         Mas não se preocupe! Você pode retornar à nossa aplicação! 😉
       </p>
