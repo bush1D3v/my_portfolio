686c5e0dcdb496dea29dd0cebc92fad8da9de7ae
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-02 23:11:50 -0300

    update(page.tsx): Alterando pagina para SSR

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,28 +1,14 @@
-"use client";
-
 import { type ReactElement } from "react";
 
-import { loaderSessionStorage } from "@/functions";
-
-import Loading from "../loading";
 import { SkillCardList } from "./components";
 
-
 export default function Skills(): ReactElement {
-  const isLoading: boolean = loaderSessionStorage("skillsLoaded");
-
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <main
-          className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
-          animate-ease-in-out max-w-screen-xl m-auto p-10"
-        >
-          <SkillCardList />
-        </main>
-      )}
-    </>
+    <main
+      className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
+      animate-ease-in-out max-w-screen-xl m-auto"
+    >
+      <SkillCardList />
+    </main>
   );
 }
