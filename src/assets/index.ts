0f860b042a8817de7b683700bb7faf10ef4c3d77
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-02 23:26:04 -0300

    update(index.ts): Atualiza exportacao de imagens

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,11 +3,23 @@ import cursoEmVideo from "./curso-em-video.jpg";
 import devQuest from "./dev-quest.jpg";
 import dio from "./dio.jpg";
 import logo from "./logo.png";
+import linktree from "./linktree.jpg";
+import pokeApi from "./poke-api.jpg";
+import capsulaDoTempo from "./capsula-do-tempo.jpg";
+import robustHabitAcademia from "./robust-habit-academia.jpg";
+import tsbank from "./tsbank.jpg";
+import devorum from "./devorum.jpg";
 
 export {
   cubosAcademy,
   cursoEmVideo,
   devQuest,
   dio,
-  logo
+  logo,
+  linktree,
+  pokeApi,
+  capsulaDoTempo,
+  robustHabitAcademia,
+  tsbank,
+  devorum
 };
