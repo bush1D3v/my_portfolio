0a9ecdfffe83b52b8bb39ac702fa38fb6939f80c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-02 23:26:48 -0300

    update(index.ts): Atualiza exportacao de componentes globais

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,11 +3,15 @@ import Footer from "./Footer";
 import ThemeProvider from "./ThemeProvider/index";
 import Modal from "./Modal";
 import LinkButton from "./LinkButton";
+import ArrowUp from "./ArrowUp";
+import ImageWithHiddenDescription from "./ImageWithHiddenDescription";
 
 export {
   Header,
   Footer,
   ThemeProvider,
   Modal,
-  LinkButton
+  LinkButton,
+  ArrowUp,
+  ImageWithHiddenDescription
 };
