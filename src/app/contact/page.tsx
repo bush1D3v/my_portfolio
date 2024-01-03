4c8393728dd3edb6f5daec06000310bd40fc4a30
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-03 00:28:00 -0300

    update(page.tsx): Atualiza componente de contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,11 +7,11 @@ export default function Contact(): ReactElement {
   return (
     <main
       className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
-      animate-ease-in-out max-w-screen-xl m-auto"
+      animate-ease-in-out max-w-screen-xl m-auto px-5 py-10"
     >
       <div
         className="flex flex-col lg:flex-row gap-20 w-fit bg-Black2 px-5 py-10 md:p-20 rounded-3xl
-            duration-200 ease-in items-center"
+        duration-200 ease-in items-center"
       >
         <TextArea />
         <Form />
