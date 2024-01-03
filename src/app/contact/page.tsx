20626822e6708fc0b673afe8f6303b69154836c2
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-03 17:56:13 -0300

    fix(page.tsx): Concerta geracao de loading da pagina contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
-import { type ReactElement } from "react";
+import { Suspense, type ReactElement } from "react";
 
 import { Form, TextArea } from "./components";
+import Loading from "../loading";
 
 export default function Contact(): ReactElement {
 
@@ -9,13 +10,15 @@ export default function Contact(): ReactElement {
       className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
       animate-ease-in-out max-w-screen-xl m-auto px-5 py-10"
     >
-      <div
-        className="flex flex-col lg:flex-row gap-20 w-fit bg-Black2 px-5 py-10 md:p-20 rounded-3xl
+      <Suspense fallback={<Loading />}>
+        <div
+          className="flex flex-col lg:flex-row gap-20 w-fit bg-Black2 px-5 py-10 md:p-20 rounded-3xl
         duration-200 ease-in items-center"
-      >
-        <TextArea />
-        <Form />
-      </div>
-    </main>
+        >
+          <TextArea />
+          <Form />
+        </div>
+      </Suspense>
+    </main >
   );
 }
