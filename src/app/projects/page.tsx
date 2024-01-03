d4329b3d6c323e779af626a9a691b5ab48fee4d7
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-03 17:56:26 -0300

    fix(page.tsx): Concerta geracao de loading da pagina projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
-import { type ReactElement } from "react";
+import { Suspense, type ReactElement } from "react";
 
 import { ProjectList } from "./components";
+import Loading from "../loading";
 
 export default function Projects(): ReactElement {
   return (
@@ -8,7 +9,9 @@ export default function Projects(): ReactElement {
       className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
       animate-ease-in-out max-w-screen-xl m-auto"
     >
-      <ProjectList />
+      <Suspense fallback={<Loading />}>
+        <ProjectList />
+      </Suspense>
     </main>
   );
 }
