dc1caf8593afb6ff2c36cd59763788e581a845fe
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-02 23:20:46 -0300

    update(page.tsx): Cria pagina padrao de projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,26 +1,14 @@
-"use client";
-
 import { type ReactElement } from "react";
 
-import { loaderSessionStorage } from "@/functions";
-
-import Loading from "../loading";
+import { ProjectList } from "./components";
 
 export default function Projects(): ReactElement {
-  const isLoading: boolean = loaderSessionStorage("projectsLoaded");
-
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <main
-          className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
-          animate-ease-in-out max-w-screen-xl p-10 m-auto"
-        >
-          <h1>Projetos</h1>
-        </main>
-      )}
-    </>
+    <main
+      className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
+      animate-ease-in-out max-w-screen-xl m-auto"
+    >
+      <ProjectList />
+    </main>
   );
 }
