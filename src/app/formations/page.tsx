5da47c20b30bf63b4fe1f05a5472ac21f1baae14
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-02 23:21:46 -0300

    refactor(page.tsx): Remove padding da pagina

--- src/app/formations/page.tsx
+++ src/app/formations/page.tsx
@@ -6,7 +6,7 @@ export default function Formations(): ReactElement {
   return (
     <main
       className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
-      animate-ease-in-out max-w-screen-xl p-10 m-auto"
+      animate-ease-in-out max-w-screen-xl m-auto"
     >
       <FormationsList />
     </main>
