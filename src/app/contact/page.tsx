4e71e3ce70b78dac216247dbc2e05b6f87c47de1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-04 15:12:50 -0300

    update(page.tsx): Atualiza componente de contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
-import { Suspense, type ReactElement } from "react";
+import { type ReactElement } from "react";
 
 import { Form, TextArea } from "./components";
-import Loading from "../loading";
 
 export default function Contact(): ReactElement {
 
@@ -10,15 +9,13 @@ export default function Contact(): ReactElement {
       className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
       animate-ease-in-out max-w-screen-xl m-auto px-5 py-10"
     >
-      <Suspense fallback={<Loading />}>
-        <div
-          className="flex flex-col lg:flex-row gap-20 w-fit bg-Black2 px-5 py-10 md:p-20 rounded-3xl
+      <div
+        className="flex flex-col lg:flex-row gap-20 w-fit bg-Black2 px-5 py-10 md:p-20 rounded-3xl
         duration-200 ease-in items-center"
-        >
-          <TextArea />
-          <Form />
-        </div>
-      </Suspense>
+      >
+        <TextArea />
+        <Form />
+      </div>
     </main >
   );
 }
