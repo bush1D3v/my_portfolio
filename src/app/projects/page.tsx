385197341452c00fa83654bc91eea8a673c2bb72
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-01-12 18:50:26 -0300

    update(page.tsx): Adicionando Suspense ao redor do componente

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,19 @@
-import { type ReactElement } from "react";
+import { Suspense, type ReactElement } from "react";
+import { unstable_noStore } from "next/cache";
 
 import { ProjectList } from "./components";
+import Loading from "../loading";
 
 export default function Projects(): ReactElement {
+  unstable_noStore();
   return (
-    <main
-      className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
+    <Suspense fallback={<Loading />}>
+      <main
+        className="flex items-center justify-center min-h-[69dvh] md:min-h-[82dvh] lg:min-h-[76dvh] animate-fade-down
       animate-ease-in-out max-w-screen-xl m-auto"
-    >
-      <ProjectList />
-    </main>
+      >
+        <ProjectList />
+      </main>
+    </Suspense>
   );
 }
