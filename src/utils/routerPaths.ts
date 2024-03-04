0e7453b2950a944afc7a8d7ee175398c7354b64c
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 22:57:26 -0300

    feat: Adiciona variaveis de caminhos da aplicacao

---
+++ src/utils/routerPaths.ts
@@ -0,0 +1,6 @@
+export const HOME: string = "/";
+export const ABOUT: string = "/about";
+export const SKILLS: string = "/skills";
+export const FORMATIONS: string = "/formations";
+export const PROJECTS: string = "/projects";
+export const CONTACT: string = "/contact";
