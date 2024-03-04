370d0399905abffcabccc54e936c0972bcb54738
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-03-03 22:21:36 -0300

    update: Adiciona valores de aria-label

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -20,12 +20,35 @@ export default function Footer(): ReactElement {
       <Addresses />
       <address>
         <ul className="flex gap-5">
-          <LinkItem href="https://github.com/bush1D3v" icon={<FaGithub />} />
-          <LinkItem href="https://www.linkedin.com/in/vj021/" icon={<FaLinkedin />} />
-          <LinkItem href="https://discord.com/users/443480311066656770" icon={<FaDiscord />} />
-          <LinkItem href="https://www.instagram.com/victorj.021" icon={<FaInstagram />} />
-          <LinkItem href="https://api.whatsapp.com/send?phone=5521995727079" icon={<FaWhatsapp />} />
-          <LinkItem href="https://twitter.com/victorj_021" icon={<FaTwitter />} />
+          <LinkItem
+            href="https://github.com/bush1D3v"
+            icon={<FaGithub />}
+            ariaLabel="Github Link"
+          />
+          <LinkItem
+            href="https://www.linkedin.com/in/vj021/"
+            icon={<FaLinkedin />}
+            ariaLabel="Linkedin Link"
+          />
+          <LinkItem
+            href="https://discord.com/users/443480311066656770" icon={<FaDiscord />}
+            ariaLabel="Discord Link"
+          />
+          <LinkItem
+            href="https://www.instagram.com/victorj.021"
+            icon={<FaInstagram />}
+            ariaLabel="Instagram Link"
+          />
+          <LinkItem
+            href="https://api.whatsapp.com/send?phone=5521995727079"
+            icon={<FaWhatsapp />}
+            ariaLabel="WhatsApp Link"
+          />
+          <LinkItem
+            href="https://twitter.com/victorj_021"
+            icon={<FaTwitter />}
+            ariaLabel="Twitter Link"
+          />
         </ul>
       </address>
     </footer>
