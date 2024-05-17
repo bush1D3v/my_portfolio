de99d78bfe5366f4c3e0b5dd83abb4ae9ef330a8
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-16 23:44:17 -0300

    refactor: Atualiza titulo

--- src/app/about/components/TextPresentation.tsx
+++ src/app/about/components/TextPresentation.tsx
@@ -6,7 +6,7 @@ export default function TextPresentation(): ReactElement {
       <h2 className="whitespace-nowrap duration-75 ease-in">VICTOR NAVARRO</h2>
       <p className="text-White mb-10 duration-75 ease-in font-bold">DESENVOLVEDOR DE SOFTWARE</p>
       <p className="leading-loose text-justify duration-75 ease-in font-semibold">
-        Sou um desenvolvedor de software curioso e autodidata, sempre disposto a aprender novas formas de melhorar
+        Sou um Engenheiro de Software curioso e autodidata, sempre disposto a aprender novas formas de melhorar
         meus métodos e aplicações de conhecimento. Através dos meus estudos venho aperfeiçoando meu lado técnico,
         comportamental e psicológico, buscando além do aprimoramento dos meus códigos, aprimorar a minha pessoa
         como um todo, indo rumo à excelência. Meus dois maiores sonhos são relacionados a minha carreira, onde o
