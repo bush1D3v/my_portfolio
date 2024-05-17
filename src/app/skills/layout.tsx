8a41847c3fcfb122b414e7bd2c70c4ca140b3e7e
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-16 23:45:34 -0300

    feat: Adiciona metadados a pagina

---
+++ src/app/skills/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import { type ReactElement } from "react";
+
+export const metadata: Metadata = {
+  title: "Habilidades | Victor navarro",
+  description: "Habilidades de Victor Navarro (bush1D3v), Engenheiro de Software"
+};
+
+export default function SkillsLayout({
+  children
+}: {
+  children: React.ReactNode;
+}): ReactElement {
+  return <>{children}</>;
+}
