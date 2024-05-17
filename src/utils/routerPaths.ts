3ddb58c3b17eac74ba094c204c68ec3e0640bdc9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-16 23:42:23 -0300

    update: Adiciona rota de experiencias

--- src/utils/routerPaths.ts
+++ src/utils/routerPaths.ts
@@ -4,3 +4,4 @@ export const SKILLS: string = "/skills";
 export const FORMATIONS: string = "/formations";
 export const PROJECTS: string = "/projects";
 export const CONTACT: string = "/contact";
+export const EXPERIENCES: string = "/experiences";
