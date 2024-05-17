07c403c924d05a62daf42adf5c64a1ae2f79d1a9
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-16 23:46:10 -0300

    update: Atualiza responsividade

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ import { NavBar } from "./components";
 export default function Header(): ReactElement {
   return (
     <header
-      className="bg-Black2 md:h-[9dvh] xl:rounded-xl lg:mt-[3dvh] max-w-screen-xl animate-fade-down animate-ease-in-out
-      duration-200 ease-in m-auto lg:mx-8 xl:mx-auto overflow-x-hidden"
+      className="bg-Black2 min-[900px]:h-[9dvh] xl:rounded-xl lg:mt-[3dvh] max-w-screen-xl
+      animate-fade-down animate-ease-in-out duration-200 ease-in m-auto lg:mx-8 xl:mx-auto overflow-x-hidden"
     >
       <NavBar />
     </header>
