81ececf08d8b8125a47d52510a0bcc7d57ba6814
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-05-16 23:43:30 -0300

    refactor: Refatorando animacao sequencial

--- src/app/skills/components/SkillCardList/components/SkillCard.tsx
+++ src/app/skills/components/SkillCardList/components/SkillCard.tsx
@@ -20,12 +20,12 @@ export default function SkillCard({
   return (
     <li
       className="relative md:static bg-Black2 hover:bg-transparent border-transparent border-2
-      hover:border-White rounded-3xl duration-200 ease-in flex flex-col p-12 text-center cursor-pointer gap-10"
+      hover:border-White rounded-3xl duration-200 ease-in flex flex-col px-4 py-10 text-center cursor-pointer gap-12"
       onClick={!isModalOpen ? (): void => setIsModalOpen(!isModalOpen) : (): string => "nada"}
     >
       <Modal keys={details} isOpen={isModalOpen} setOpen={setIsModalOpen} />
-      <span className="text-8xl text-White px-20">{icon}</span>
-      <h3>{title}</h3>
+      <span className="text-8xl text-White px-20 duration-100 ease-in">{icon}</span>
+      <h3 className="duration-100 ease-in">{title}</h3>
     </li >
   );
 }
