9b5ddee3241b1a72a995154ec2a276ebe6784964
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-10-19 15:35:28 -0300

    update: Alter techs experiences

--- src/app/skills/components/SkillCardList/SkillCardList.tsx
+++ src/app/skills/components/SkillCardList/SkillCardList.tsx
@@ -1,21 +1,15 @@
 import { type ReactElement } from "react";
 import {
   SiJest,
-  SiReact,
   SiTypescript,
   SiNodedotjs,
   SiNextdotjs,
-  SiTailwindcss,
-  SiSass,
   SiPostgresql,
   SiPhp,
-  SiJavascript,
-  SiHtml5,
-  SiCss3,
-  SiExpress,
   SiNestjs,
   SiActix,
-  SiRedis
+  SiRedis,
+  SiVuedotjs
 } from "react-icons/si";
 import { TbBrandReactNative } from "react-icons/tb";
 import { FaRust } from "react-icons/fa6";
@@ -25,52 +19,6 @@ import { SkillCard } from "./components";
 export default function SkillCardList(): ReactElement {
   return (
     <ul className="flex justify-center gap-8 flex-wrap p-10">
-      <SkillCard
-        icon={<SiNextdotjs />}
-        title={"Next"}
-        details={[
-          "SSR",
-          "SSG",
-          "API",
-          "SEO",
-          "Routing",
-        ]}
-      />
-      <SkillCard
-        icon={<SiNestjs />}
-        title={"Nest"}
-        details={[
-          "Decorators",
-          "Clean Arch",
-          "Modules",
-          "SOLID",
-          "POO",
-        ]}
-      />
-      <SkillCard
-        icon={<TbBrandReactNative />}
-        title={"React Native"}
-        details={[
-          "Integração de APIs",
-          "Módulos Nativos",
-          "Fundamentals",
-          "Navigation",
-          "Contextos",
-          "Estados",
-        ]}
-      />
-      <SkillCard
-        icon={<SiActix />}
-        title={"Actix"}
-        details={[
-          "Swagger Docs",
-          "Modules",
-          "RESTful",
-          "Testing",
-          "MVC",
-          "POO",
-        ]}
-      />
       <SkillCard
         icon={<SiTypescript />}
         title={"TypeScript"}
@@ -108,6 +56,31 @@ export default function SkillCardList(): ReactElement {
         ]}
       />
       <SkillCard
+        icon={<SiVuedotjs />}
+        title={"Vue.js"}
+        details={[
+          "Props",
+          "Hooks",
+          "Eventos",
+          "V-model",
+          "Estados",
+          "Componentes",
+          "Ciclo de vida",
+        ]}
+      />
+      <SkillCard
+        icon={<TbBrandReactNative />}
+        title={"React Native"}
+        details={[
+          "Integração de APIs",
+          "Módulos Nativos",
+          "Fundamentals",
+          "Navigation",
+          "Contextos",
+          "Estados",
+        ]}
+      />
+      <SkillCard
         icon={<SiRedis />}
         title={"Redis"}
         details={[
@@ -130,8 +103,42 @@ export default function SkillCardList(): ReactElement {
         ]}
       />
       <SkillCard
+        icon={<SiNextdotjs />}
+        title={"Next.js"}
+        details={[
+          "SSR",
+          "SSG",
+          "API",
+          "SEO",
+          "Routing",
+        ]}
+      />
+      <SkillCard
+        icon={<SiNestjs />}
+        title={"Nest"}
+        details={[
+          "Decorators",
+          "Clean Arch",
+          "Modules",
+          "SOLID",
+          "POO",
+        ]}
+      />
+      <SkillCard
+        icon={<SiActix />}
+        title={"Actix"}
+        details={[
+          "Swagger Docs",
+          "Modules",
+          "RESTful",
+          "Testing",
+          "MVC",
+          "POO",
+        ]}
+      />
+      <SkillCard
         icon={<SiNodedotjs />}
-        title={"Node"}
+        title={"Node.js"}
         details={[
           "API RESTful",
           "Protocolo HTTP",
@@ -150,86 +157,6 @@ export default function SkillCardList(): ReactElement {
           "Testes de Unidade",
           "Testes Assíncronos",
           "Testes de Integração",
-        ]}
-      />
-      <SkillCard
-        icon={<SiTailwindcss />}
-        title={"Tailwind"}
-        details={[
-          "Animations",
-          "Mobile First",
-          "Estrutura CSS Utilitária",
-          "Componentes Reutilizáveis",
-          "Classes de Estilo Pré-definidas"
-        ]}
-      />
-      <SkillCard
-        icon={<SiExpress />}
-        title={"Express"}
-        details={[
-          "Criptografia",
-          "Jest Testing",
-          "Clean Arch",
-          "Swagger",
-          "CORS",
-        ]}
-      />
-      <SkillCard
-        icon={<SiReact />}
-        title={"React"}
-        details={[
-          "Props",
-          "Hooks",
-          "Estados",
-          "Componentes",
-          "Ciclo de vida",
-        ]}
-      />
-      <SkillCard
-        icon={<SiSass />}
-        title={"SASS"}
-        details={[
-          "Mixins",
-          "Partials",
-          "Funções SASS",
-          "Variáveis SASS",
-          "Herança de Seletores",
-          "Aninhamento de Regras"
-        ]}
-      />
-      <SkillCard
-        icon={<SiHtml5 />}
-        title={"HTML5"}
-        details={[
-          "Eventos",
-          "Semântica",
-          "Atributos",
-          "Elementos",
-          "Formulários",
-          "Acessibilidade"
-        ]}
-      />
-      <SkillCard
-        icon={<SiCss3 />}
-        title={"CSS3"}
-        details={[
-          "Cascata",
-          "Herança",
-          "Animações",
-          "Box Model",
-          "Flexbox/Grid",
-          "Especificidade",
-          "Media Queries"
-        ]}
-      />
-      <SkillCard
-        icon={<SiJavascript />}
-        title={"JavaScript"}
-        details={[
-          "DOM",
-          "POO",
-          "Promises",
-          "Fetch API",
         ]}
       />
     </ul>
